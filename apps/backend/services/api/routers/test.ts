1c7725e31c95313d57acc2b6c99feba2f93fa63c
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-11-24 12:27:05 +0100

    Add dataset, implement parsing of dataset & update test endpoint

--- apps/backend/services/api/routers/test.ts
+++ apps/backend/services/api/routers/test.ts
@@ -1,12 +1,15 @@
 import * as z from "zod";
 import { publicProcedure } from "../index.js";
+import { getParsedBlogs } from "../../parsers/blogs.parser.js";
 
 export const testRouter = {
 	test: publicProcedure
 		.route({ method: "GET" })
 		.input(z.object({ limit: z.number().optional() }))
 		.output(z.object({ message: z.string(), timestamp: z.number() }))
-		.handler(() => {
+		.handler(async () => {
+			const blog = await getParsedBlogs();
+			console.log("blogs", blog![0]);
 			return { message: "OK", timestamp: Date.now() };
 		}),
 };
---
+++ apps/backend/utils/pierson.ts
@@ -0,0 +1,14 @@
+export function pierson(firstNode: any, secondNode: any) {
+	let sumFirstNode: number = 0;
+	let sumSecondNode: number = 0;
+
+	let sumSqrdFirstNode: number = 0;
+	let sumSqrdSecondNode: number = 0;
+
+	let matching = 706; // TODO: Update to none hard-coded value
+
+	for (let i = 0; i <= matching; i++) {
+		const countFirstNode = firstNode.wordCount(i);
+		const countSecondNode = secondNode.wordCount(i);
+	}
+}
